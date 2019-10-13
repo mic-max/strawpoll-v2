c9029e42960a25af8650d2b5412fe31ac21ffee7
Michael Maxwell <mike.ian.maxwell@gmail.com>
2019-10-13 06:12:27 -0700

    Version number from package.json in footer of each page

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,26 @@
 const express = require('express')
 const Poll = require('../models/Poll')
-const util = require('../public/js/util.js')
+const util = require('../public/js/util')
+const { version } = require('../package')
 
 const router = express.Router()
 
 /* GET home page. */
 router.get('/', (req, res) => {
-	res.render('index')
+	res.render('index', {version})
 })
 
 /* GET poll page. */
 router.get('/:id', (req, res) => {
 	Poll.findById(req.params.id)
-		.then((poll) => res.render('poll', poll))
+		.then((poll) => res.render('poll', Object.assign(poll, {version})))
 		.catch(() => res.sendStatus(404))
 })
 
 /* GET poll results page. */
 router.get('/:id/r', (req, res) => {
 	Poll.findById(req.params.id)
-		.then((poll) => res.render('result', Object.assign(poll, util)))
+		.then((poll) => res.render('result', Object.assign(poll, util, {version})))
 		.catch(() => res.sendStatus(404))
 })
 
